3ba3f5004ae497494a9cc7b5adf0806620b00887
Rhino <rhino@localhost.localdomain>
2023-04-14 21:25:45 +0530

    Navigation Stack 1

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Routes from './src/Navigation/Routes'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+   <Routes />
   )
 }
 
---
+++ src/Navigation/Routes.js
@@ -0,0 +1,22 @@
+import {View, Text, Settings} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './Home';
+import Search from './Search';
+import Setting  from './Setting';
+
+const Stack = createNativeStackNavigator();
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Search'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Setting" component={Setting} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
